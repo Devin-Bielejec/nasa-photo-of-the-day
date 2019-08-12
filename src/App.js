7035211603a9d6bd11f8cfd24aff5e18e266561b
devin-bielejec <devin.bieleje@gmail.com>
2019-08-12 18:39:36 -0400

    Refiled components, styled TitleComponent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
+import styled from "styled-components";
 
 //component Imports
-import {TitleComponent} from "./components/TitleComponent/TitleComponent";
-import {PictureComponent} from "./components/PictureComponent/PictureComponent";
-import {ExplanationComponent} from "./components/ExplanationComponent/ExplanationComponent";
-import {DateComponent} from "./components/DateComponent/DateComponent";
-import {CopyrightComponent} from "./components/CopyrightComponent/CopyrightComponent";
+import {TitleComponent} from "./components/TitleComponent";
+import {PictureComponent} from "./components/PictureComponent";
+import {ExplanationComponent} from "./components/ExplanationComponent";
+import {DateComponent} from "./components/DateComponent";
+import {CopyrightComponent} from "./components/CopyrightComponent";
+
+const Body = styled.body`
+  width: 75%;
+  margin: 0 auto;
+  font-size: 1rem;
+  color: red;
+`
 
 function App() {
   const [info, setInfo] = useState("");
@@ -25,13 +33,13 @@ function App() {
   console.log("data",info);
   console.log(info.copyright)
   return (
-    <div className="App">
+    <Body>
       <TitleComponent title={info.title}/>
       <PictureComponent picSrc={info.url}/>
       <ExplanationComponent explanation={info.explanation}/>
       <DateComponent date={info.date}/>
       <CopyrightComponent copyright={info.copyright}/>
-    </div>
+    </Body>
   );
 }
 
--- src/components/TitleComponent/TitleComponent.js
+++
@@ -1,9 +0,0 @@
-import React, {useState} from "react";
-
-function TitleComponent(props) {
-    return (
-        <h1>{props.title}</h1>
-    );
-}
-
-export {TitleComponent};
