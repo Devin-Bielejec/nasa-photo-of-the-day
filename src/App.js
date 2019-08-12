01d87652d062d943a653bd8e33fc0fed32e24655
devin-bielejec <devin.bieleje@gmail.com>
2019-08-12 17:37:30 -0400

    Fetched data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
   const [data, changeData] = useState();
+
+  useEffect( () => {
+    const fetchData = () => {
+      axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .then(res => console.log(res));
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <p>
