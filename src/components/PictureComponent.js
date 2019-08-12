79a2a1127c4bb8d0b38dd8fc87a95768c679fd04
devin-bielejec <devin.bieleje@gmail.com>
2019-08-12 18:44:33 -0400

    styled picture component

--- src/components/PictureComponent.js
+++ src/components/PictureComponent.js
@@ -1,8 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const Img = styled.img`
+    border-radius: 70px;
+    box-shadow: 10px 10px gray;
+    margin: 0 auto;
+    width: 75%;
+`
+
+const ImgDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`
 
 function PictureComponent(props) {
     return (
-        <img src={props.picSrc}/>
+        <ImgDiv>
+            <Img src={props.picSrc} alt={props.alt}/>
+        </ImgDiv>
     );
 }
 
